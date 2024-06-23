ff5c201e1155f4dd331b7e580c7f655af986abbf
Hemlock4414 <antonio.marrara@gmx.ch>
2024-06-23 16:50:39 +0200

    update

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function feedbackGood() {
     document.getElementById("reply-good").innerHTML = "Thank you very much, that makes me happy!";
 }
 
-const myImg = document.getElementById("smileNeutral");
+const myImg = document.getElementById("smileStart");
 const btnBad = document.getElementById("btnBad");
 const btnGood = document.getElementById("btnGood");
 
 btnBad.addEventListener("click", function() {
-  myImg.src = "smiley-x-eyes.svg";
+  myImg.src = "emoji-dizzy.svg";
 });
 
 btnGood.addEventListener("click", function() {
